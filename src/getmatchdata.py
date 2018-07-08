50876c996fcd2420d4c8be9805b4ce59539f7eb7
Rishabh Patil <rishabh@particle41.com>
2018-07-08 23:47:35 +0530

    Check for "rate limit exceeded" error

--- src/getmatchdata.py
+++ src/getmatchdata.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import time
 
 matches = os.listdir("matches")
 for i in range(len(matches)):
@@ -14,8 +15,14 @@ def get_match_data(match_id):
     else:
         r = requests.get("https://api.opendota.com/api/matches/"+str(match_id))
         result = r.json()
+        if "error" in result and result["error"]=="rate limit exceeded":
+            return 1
+        elif "error" in result:
+            print(result)
+            exit()
         with open("matches/"+str(match_id),"w+") as f:
             json.dump(result, f)
+        return 1
 
 if __name__=="__main__":
     with open("result.json","r") as f:
@@ -24,7 +31,12 @@ if __name__=="__main__":
     print(len(data["match_list"]))
     new_set = set(data["match_list"]) - set(matches)
     print(len(new_set))
+    count = 0
     for match in new_set:
-        print(match)
-        get_match_data(match)
-    
+        error = -1
+        count+=1
+        print(count,match)
+        while error==-1:
+            error = get_match_data(match)
+            if error==-1:
+                time.sleep(10)
