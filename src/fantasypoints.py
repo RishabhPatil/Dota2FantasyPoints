998fe4840f5abb7dc38cc03ecd56782b85890358
Rishabh Patil <rishabh@particle41.com>
2018-07-09 22:15:09 +0530

    Matches with certain missing data are excluded.

--- src/fantasypoints.py
+++ src/fantasypoints.py
@@ -23,41 +23,56 @@ for mid in matches:
         
     for index in range(len(data["players"])):
         i = data["players"][index]
+
+        if None in [i["towers_killed"],i["roshans_killed"],i["teamfight_participation"],i["obs_placed"],i["camps_stacked"],i["rune_pickups"],i["firstblood_claimed"],i["stuns"]]:
+            continue
+
         id = i["account_id"]
         if id not in players:
             players[id] = { "fantasy_points":{} }
-            players[id]["fantasy_points"]["kill_points"] = 0
-            players[id]["fantasy_points"]["death_points"] = 0
-            players[id]["fantasy_points"]["LHD_points"] = 0
-            players[id]["fantasy_points"]["tower_points"] = 0
-            players[id]["fantasy_points"]["roshan_points"] = 0
-            players[id]["fantasy_points"]["teamfight_points"] = 0
-            players[id]["fantasy_points"]["wards_points"] = 0
-            players[id]["fantasy_points"]["camp_points"] = 0
-            players[id]["fantasy_points"]["rune_points"] = 0
-            players[id]["fantasy_points"]["fb_points"] = 0
-            players[id]["fantasy_points"]["stun_points"] = 0
+            players[id]["fantasy_points"]["kill_points"] = float(0)
+            players[id]["fantasy_points"]["death_points"] = float(0)
+            players[id]["fantasy_points"]["LHD_points"] = float(0)
+            players[id]["fantasy_points"]["tower_points"] = float(0)
+            players[id]["fantasy_points"]["roshan_points"] = float(0)
+            players[id]["fantasy_points"]["teamfight_points"] = float(0)
+            players[id]["fantasy_points"]["wards_points"] = float(0)
+            players[id]["fantasy_points"]["camp_points"] = float(0)
+            players[id]["fantasy_points"]["rune_points"] = float(0)
+            players[id]["fantasy_points"]["fb_points"] = float(0)
+            players[id]["fantasy_points"]["stun_points"] = float(0)
             players[id]["total_matches"] = 0
-            players[id]["teams"] = set()
-            players[id]["names"] = set()
+            players[id]["teams"] = []
+            players[id]["names"] = []
 
         players[id]["fantasy_points"]["kill_points"] += i["kills"]*0.3
         players[id]["fantasy_points"]["death_points"] += (3 - (i["deaths"]*0.3))
         players[id]["fantasy_points"]["LHD_points"] += 0.003* (i["last_hits"] + i["denies"])
-        players[id]["fantasy_points"]["tower_points"] += i["towers_killed"]
-        players[id]["fantasy_points"]["roshan_points"] += i["roshans_killed"]
-        players[id]["fantasy_points"]["teamfight_points"] += i["teamfight_participation"]*3
-        players[id]["fantasy_points"]["wards_points"] += i["obs_placed"]*0.5
-        players[id]["fantasy_points"]["camp_points"] += i["camps_stacked"]*0.5
-        players[id]["fantasy_points"]["rune_points"] += i["rune_pickups"]*0.25
-        players[id]["fantasy_points"]["fb_points"] += i["firstblood_claimed"]
-        players[id]["fantasy_points"]["stun_points"] += i["stuns"]
-        players[id]["names"].add(i["name"])
-
+        if i["towers_killed"] is not None:
+            players[id]["fantasy_points"]["tower_points"] += i["towers_killed"]
+        if i["roshans_killed"] is not None:
+            players[id]["fantasy_points"]["roshan_points"] += i["roshans_killed"]
+        if i["teamfight_participation"] is not None:
+            players[id]["fantasy_points"]["teamfight_points"] += i["teamfight_participation"]*3
+        if i["obs_placed"] is not None:
+            players[id]["fantasy_points"]["wards_points"] += i["obs_placed"]*0.5
+        if i["camps_stacked"] is not None:
+            players[id]["fantasy_points"]["camp_points"] += i["camps_stacked"]*0.5
+        if i["rune_pickups"] is not None:
+            players[id]["fantasy_points"]["rune_points"] += i["rune_pickups"]*0.25
+        if i["firstblood_claimed"] is not None:
+            players[id]["fantasy_points"]["fb_points"] += i["firstblood_claimed"]
+        if i["stuns"] is not None:
+            players[id]["fantasy_points"]["stun_points"] += i["stuns"]
+        if i["name"] not in players[id]["names"]:
+            players[id]["names"].append(i["name"])
+        players[id]["total_matches"] += 1
         if index<=4:
-            players[id]["teams"].add(radiant)
+            if radiant not in players[id]["teams"]: 
+                players[id]["teams"].append(radiant)
         else:
-            players[id]["teams"].add(dire)
+            if dire not in players[id]["teams"]:
+                players[id]["teams"].append(dire)
 
 with open("players.json","w") as f:
     json.dump(players,f)
